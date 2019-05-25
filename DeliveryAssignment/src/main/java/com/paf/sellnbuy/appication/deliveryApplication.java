f716d6aadab53cdf60d513d308f04460d7f57ab8
AchinthaTM <achinthatennakoon01@gmail.com>
2019-05-25 22:26:21 +0530

    PAF Assignment

---
+++ DeliveryAssignment/src/main/java/com/paf/sellnbuy/appication/deliveryApplication.java
@@ -0,0 +1,13 @@
+package com.paf.sellnbuy.appication;
+
+import org.springframework.boot.SpringApplication;
+
+import com.paf.sellnbuy.SellNBuyApplication;
+
+public class deliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellNBuyApplication.class, args);
+	}
+
+}
---
+++ DeliveryAssignment/src/main/java/com/paf/sellnbuy/repository/deliveryRepository.java
@@ -0,0 +1,8 @@
+package com.paf.sellnbuy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface deliveryRepository extends JpaRepository<Delivery, Integer>{
+
+}
